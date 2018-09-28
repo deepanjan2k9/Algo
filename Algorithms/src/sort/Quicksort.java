bcab6dd4a848bbdd27a6735722a32cca64e0c92e
Deepanjan Das <deepanjan.2k9@gmail.com>
2018-09-28 01:50:14 -0400

    Added skeleton code

--- Algorithms/src/sort/Quicksort.java
+++ Algorithms/src/sort/Quicksort.java
@@ -4,10 +4,36 @@ package sort;
  * Class to implement quick sort algorithm
  * */
 public class Quicksort {
-
+	
+	int[] array;
+	
+	public Quicksort(int[] arr) {
+		array = arr;
+	}
+	
+	public void QuicksortFirstElementPivot() {
+		
+	}
+	
+	public void QuicksortLastElementPivot() {
+		
+	}
+	
+	public void QuicksortMedianElementPivot() {
+		
+	}
+	
+	public void QuicksortRandomElementPivot() {
+		
+	}
+	
+	public void Partition() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
